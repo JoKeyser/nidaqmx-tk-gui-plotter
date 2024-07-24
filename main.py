90c83644559f8376c50351218e0d616df7686928
Johannes Keyser <johannes.keyser@sport.uni-giessen.de>
2024-07-24 14:23:22 +0200

    Wrestling with the acquisition buffer overflow; plot updates seem too slow

--- main.py
+++ main.py
@@ -12,18 +12,23 @@
 import nidaqmx
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 from nidaqmx.constants import AcquisitionType, TerminalConfiguration
 from nidaqmx.stream_readers import AnalogMultiChannelReader
 
+# Set desired parameters for data acquisition and plotting.
 READ_CHANNELS = ["Dev1/ai0", "Dev1/ai1", "Dev1/ai2"]  # channel(s) to read from  # FIXME: disentangle DevX from channel numbers
-READ_RATE = 1000  # reading/sampling rate in Hz
-SAMPS_PER_CHAN = 100  # number of samples to acquire per channel into the PC buffer
-NUMBER_OF_SAMPLES_PER_CHANNEL = 10  # number of samples to read per channel from PC buffer
-PLOT_INTERVAL = 0.01  # interval between plot updates (in seconds)
-HISTORY_DURATION = 3  # in seconds
-HISTORY_LENGTH = int(READ_RATE * HISTORY_DURATION)
+READ_RATE = 3000  # acquisition sampling rate, in Hz
+HISTORY_DURATION = 3  # history duration, in seconds
+PLOT_REFRESH_RATE = 20  # refresh rate of the plot, in Hz
 
-# FIXME: Record and plot a fixed amount of history. Works for a bit, then crashes:
+# Calculate derived parameters.
+HIST_LEN = int(READ_RATE * HISTORY_DURATION)  # number of samples in history
+PLOT_INTERVAL = 1 / PLOT_REFRESH_RATE  # plot update interval, in seconds
+# For real-time plotting, the plot refresh rate is the limiting factor;
+# i.e., it is unnecessary to acquire data faster than the plot refresh rate.
+#
+# FIXME: For PLOT_REFRESH_RATE > ~10 Hz, the following error occurs:
 #     "Exception has occurred: DaqReadError -200279"
 #     "The application is not able to keep up with the hardware acquisition."
 #     "Increasing the buffer size, reading the data more frequently,
@@ -48,22 +53,25 @@ HISTORY_LENGTH = int(READ_RATE * HISTORY_DURATION)
 # Both steps must agree: The data rate from hardware to the PC buffer must
 # not be too fast nor too slow compared to the data rate into the hardware.
 #
-# - It seems to work now, but does it really? Need to understand the
-#   underlying mechanisms better.
-#     - Check/get the hardware's rate, which may be different from the
-#       "user-requested" rate. Set the rest of parameters accordingly.
+# Thus, set the buffer size for continuous acquisition to a multiple of the
+# plotting rate.
+SAMPS_PER_CHAN = int(READ_RATE / PLOT_REFRESH_RATE * 2)
+# Set the number of samples per plot refresh from the buffer to be slightly
+# larger than what is expected to be in the buffer at that time, to avoid
+# buffer overflows by too much acquisition over too little consumption.
+NUMBER_OF_SAMPLES_PER_CHANNEL = int(READ_RATE / PLOT_REFRESH_RATE * 1.1)
 
 # initialize variables for data storage and plotting
-read_data = np.zeros([len(READ_CHANNELS), SAMPS_PER_CHAN])  # most recently read data
-data_history = np.nan * np.zeros([len(READ_CHANNELS), HISTORY_LENGTH])  # history of read data
+data_read = np.zeros([len(READ_CHANNELS), NUMBER_OF_SAMPLES_PER_CHANNEL])
+data_hist = np.nan * np.zeros([len(READ_CHANNELS), HIST_LEN])  # history of read data
 # generate x-values based on linspace, and repeat the same array for all channels
-xvalues = np.linspace(-HISTORY_LENGTH, 0, HISTORY_LENGTH)  # FIXME: For now, in samples, not in seconds
+xvalues = np.linspace(-HIST_LEN, 0, HIST_LEN)  # FIXME: For now, in samples, not in seconds
 xvalues = np.tile(xvalues, [len(READ_CHANNELS), 1])
 
 # create an interactive plotting window
 plt.ion()
 fig, axs = plt.subplots()
-lines = axs.plot(xvalues.T, data_history.T)
+lines = axs.plot(xvalues.T, data_hist.T)
 axs.set_xlabel('History of samples (newest on the right ->)')
 axs.set_ylabel('Voltage [V]')
 axs.set_title(f"Real-time plot, sampling rate = {READ_RATE} Hz")
@@ -82,14 +90,19 @@ with nidaqmx.Task() as read_task:
     read_task.start()
     
     while True:
-        reader.read_many_sample(data = read_data, 
-                                number_of_samples_per_channel = SAMPS_PER_CHAN)
+        reader.read_many_sample(data = data_read, 
+            number_of_samples_per_channel = NUMBER_OF_SAMPLES_PER_CHANNEL)
         # shift the history to the left and append the new data
-        data_history = np.roll(data_history, -SAMPS_PER_CHAN, axis=1)
-        data_history[:, -SAMPS_PER_CHAN:] = read_data
+        data_hist = np.roll(data_hist, -NUMBER_OF_SAMPLES_PER_CHANNEL, axis=1)
+        data_hist[:, -NUMBER_OF_SAMPLES_PER_CHANNEL:] = data_read
+        
         # update the plot
         for idx, line in enumerate(lines):
-            line.set_ydata(data_history[idx, :].T)
+            line.set_ydata(data_hist[idx, :].T)
         axs.relim()
         axs.autoscale_view()
-        plt.pause(PLOT_INTERVAL)  # refresh the plot
+        time_start = time.time()
+        # FIXME: pause() takes about >0.1 seconds even for shorter intervals, leading to buffer overflows
+        plt.pause(PLOT_INTERVAL)
+        time_stop = time.time()
+        print(f"Time since update = {time_stop - time_start:.3f} s")
